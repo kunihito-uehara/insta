a53d2f2edfe8b79bc464757040511488b97f33f3
kunihitouehara <uekuni2000@gmail.com>
2021-08-04 14:47:28 +0900

    CSS変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_052658) do
+ActiveRecord::Schema.define(version: 2021_08_04_023146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_08_03_052658) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
