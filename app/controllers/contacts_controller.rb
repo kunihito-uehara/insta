6430a6ffd9e6e3c567e02ae8e801893b46485b69
kunihitouehara <uekuni2000@gmail.com>
2021-08-04 10:45:53 +0900

    ボタン非表示

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: %i[edit update destroy]
+  before_action :authenticate_user!, only: %i[new edit update destroy]
 
   def index
     @contacts = Contact.all
