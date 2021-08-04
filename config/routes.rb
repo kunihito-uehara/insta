341d439484686734b99a1fa3caafc8ebb9408be2
kunihitouehara <uekuni2000@gmail.com>
2021-08-04 17:27:00 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  get 'sessions/new'
+  root to: "contacts#index"
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
